d0cfb92279d6c476b56641f9755ed73d8c229fb0
Victor Fulgencio <victor.andrade4@hotmail.com>
2021-10-03 19:29:38 -0300

    change geojson standard format

--- Services/KmlFileService.cs
+++ Services/KmlFileService.cs
@@ -7,29 +7,29 @@ using System.Linq;
 
 namespace tcc_back.Services
 {
-    public class KmlFileService : CoberturaService, IKmlFileService
+  public class KmlFileService : CoberturaService, IKmlFileService
+  {
+
+    public KmlFileService(ICoberturaRepository repository) : base(repository)
+    { }
+
+    public string GetFilePath(string uf, string city)
     {
-
-        public KmlFileService(ICoberturaRepository repository) : base(repository)
-        { }
-
-        public string GetFilePath(string uf, string city)
-        {
-            var cityCode = this.GetCityCode(uf, city);
-            var cityNameWithouAccents = RemoveAccents(city);
-            return $"{cityCode}_{cityNameWithouAccents}_Setores_2020.geojson".Replace(" ", "+");
-        }
-
-        private string RemoveAccents(string text)
-        {
-            StringBuilder sbReturn = new StringBuilder();
-            var arrayText = text.Normalize(NormalizationForm.FormD).ToCharArray();
-            foreach (char letter in arrayText)
-            {
-                if (CharUnicodeInfo.GetUnicodeCategory(letter) != UnicodeCategory.NonSpacingMark)
-                    sbReturn.Append(letter);
-            }
-            return sbReturn.ToString();
-        }
+      var cityCode = this.GetCityCode(uf, city);
+      var cityNameWithouAccents = RemoveAccents(city);
+      return $"{cityCode}_{cityNameWithouAccents}_Setores_2020.geojson";
     }
+
+    private string RemoveAccents(string text)
+    {
+      StringBuilder sbReturn = new StringBuilder();
+      var arrayText = text.Normalize(NormalizationForm.FormD).ToCharArray();
+      foreach (char letter in arrayText)
+      {
+        if (CharUnicodeInfo.GetUnicodeCategory(letter) != UnicodeCategory.NonSpacingMark)
+          sbReturn.Append(letter);
+      }
+      return sbReturn.ToString();
+    }
+  }
 }
