b5102aaa69fcf2841cf4ac320d8cbf80f3e1dae1
Victor Fulgencio <victor.andrade4@hotmail.com>
2021-10-18 01:53:37 -0300

    add table cidades

--- Repositories/CoberturaRepository.cs
+++ Repositories/CoberturaRepository.cs
@@ -6,38 +6,38 @@ using Dapper;
 
 namespace tcc_back.Repositories
 {
-    public class CoberturaRepository : ICoberturaRepository
+  public class CoberturaRepository : ICoberturaRepository
+  {
+    private readonly IDbConnection _dbConnection;
+
+    public CoberturaRepository(IDbConnection dbConnection)
     {
-        private readonly IDbConnection _dbConnection;
-
-        public CoberturaRepository(IDbConnection dbConnection)
-        {
-            _dbConnection = dbConnection;
-        }
-        public IEnumerable<string> GetCities(string uf)
-        {
-            const string sql = @"SELECT DISTINCT MUNICIPIO FROM cobertura WHERE UF = (@UF) ORDER BY MUNICIPIO";
-            return _dbConnection.Query<string>(sql, new { UF = uf });
-        }
-
-        public string GetCityCode(string uf, string city)
-        {
-            const string sql = @"SELECT DISTINCT Codigo_Municipio FROM cobertura WHERE UF = (@UF) AND Municipio = (@City) LIMIT 1";
-            try
-            {
-                return _dbConnection.QuerySingle<string>(sql, new { UF = uf, City = city });
-            }
-            catch (InvalidOperationException)
-            {
-                return null;
-            }
-        }
-
-        public IEnumerable<Cobertura> GetAreas(string uf, string city)
-        {
-            const string sql = @"SELECT DISTINCT Setor_Censitario, Tipo_Setor, Bairro FROM cobertura WHERE UF = (@UF) AND Municipio = (@City)";
-            return _dbConnection.Query<Cobertura>(sql, new { UF = uf, City = city });
-        }
+      _dbConnection = dbConnection;
     }
+    public IEnumerable<string> GetCities(string uf)
+    {
+      const string sql = @"SELECT NOME FROM cidades WHERE UF = (@UF) ORDER BY NOME";
+      return _dbConnection.Query<string>(sql, new { UF = uf });
+    }
+
+    public string GetCityCode(string uf, string city)
+    {
+      const string sql = @"SELECT DISTINCT Codigo_Municipio FROM cobertura WHERE UF = (@UF) AND Municipio = (@City) LIMIT 1";
+      try
+      {
+        return _dbConnection.QuerySingle<string>(sql, new { UF = uf, City = city });
+      }
+      catch (InvalidOperationException)
+      {
+        return null;
+      }
+    }
+
+    public IEnumerable<Cobertura> GetAreas(string uf, string city)
+    {
+      const string sql = @"SELECT DISTINCT Setor_Censitario, Tipo_Setor, Bairro FROM cobertura WHERE UF = (@UF) AND Municipio = (@City)";
+      return _dbConnection.Query<Cobertura>(sql, new { UF = uf, City = city });
+    }
+  }
 
 }
